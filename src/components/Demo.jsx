7ae42dbea6844f0fa50b8d5b40030e86770eb554
Govinda Hosein <gh604721@digicelgroup.com>
2023-05-31 14:30:09 -0400

    Add Redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
+
+  return (
+    <section className="w-full max-w-xl mt-16">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action=""
+          className="relative flex items-center justify-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 w-5 my-2 ml-3"
+          />
+          <input
+            type="url"
+            placeholder="Enter a url"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({
+                ...article,
+                url: e.target.value,
+              });
+            }}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
+        </form>
+
+        {/* Browse URL History */}
+      </div>
+
+      {/* Display Results */}
+    </section>
+  );
 };
 
 export default Demo;
